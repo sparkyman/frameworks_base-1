f41f414fa57a4b2a2ec65094c3973056230c6182
davros <davros@droidconcepts.com>
2012-08-25 11:34:19 -0400

    SystemUI: Recreate navigation bar when rebuilding the status bar
    Ensure that all listeners (such as orientation change...) get properly
    linked in the newly generated view, to avoid errors due to non-attached
    views after applying a theme
    
    Change-Id: I46992694844944564739ac12a8da43b6348658c9
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2433,9 +2433,13 @@ public class PhoneStatusBar extends BaseStatusBar {
         copyNotifications(notifications, mNotificationData);
         mNotificationData.clear();
 
+        if (mNavigationBarView != null) {
+            WindowManagerImpl.getDefault().removeView(mNavigationBarView);
+        }
         makeStatusBarView();
         repositionNavigationBar();
         mNavigationBarView.updateResources();
+        addNavigationBar();
 
         // recreate StatusBarIconViews.
         for (int i = 0; i < nIcons; i++) {
